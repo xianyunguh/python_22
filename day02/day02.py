bd60e579ac677b5baf5e11b60d416ab56a61e0f5
global-zhangsan <global-email@xx.com>
2020-05-18 16:26:24 +0800

    20200518

--- day02/day02.py
+++ day02/day02.py
@@ -10,3 +10,73 @@
 
 # 6、while else:   while 循环如果被 break 打断，则不执行 else 语句。
 
+# 7、制作一个公共的模板，让一个字符串的某些位置变成动态可传入的。
+"""
+-------- info of 太白金星 --------
+name : 太白金星
+age  : 18
+job  : Teacher
+Hobbie: girl
+-------- end --------
+"""
+
+# 8、用占位符 % 输出 ”我叫 太白金星, 今年 18, 学习进度 1%" ,其中 1%不用占位符
+
+# 9、判断下面的打印结果：
+"""
+i1 = 2
+i2 = 3
+print(2 ** 3)
+print(10//3)
+print(10 % 3)
+
+print(3 != 4)
+
+count = 1
+count = count + 1
+count += 1
+print(count)
+"""
+
+# 10、 判断下面的打印结果：
+'''
+print(2 > 1 and 3 < 4 or 4 > 5 and 2 < 1)
+print(True or False)
+'''
+
+# 11、判断打印结果：
+'''
+print(1 or 2)
+print(3 or 2)
+print(4 or 2)
+print(-1 or 2)
+print(0 or 2)
+print(1 and 2)
+'''
+
+# 12、判断结果：
+'''
+s1 = '00100'
+print(int(s1))
+
+i1 = 100
+print(str(i1),type(str(i1)))
+
+i = 0
+print(bool(i))
+print(int(True)) 
+print(int(False)) 
+'''
+
+# 13、判断结果：
+'''
+print(3 and 9)
+print(1 and 2 or 3 and 4)
+'''
+
+# 14、判断结果：
+'''
+print(1 > 2 and 3 or 6)
+print(2**32)
+print(7.6 * 1024 * 1024 * 8)
+'''
